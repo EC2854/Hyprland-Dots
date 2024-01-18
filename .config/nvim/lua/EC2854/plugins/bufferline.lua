43945db2930e4381fa6c42b631853e6cae8cd914
Mleciu <igorpietrus@gmail.com>
2024-01-18 18:59:03 +0100

    lf preview and leap.nvim

--- .config/nvim/lua/EC2854/plugins/bufferline.lua
+++ .config/nvim/lua/EC2854/plugins/bufferline.lua
@@ -9,7 +9,7 @@ return {
             right_mouse_command = "Bdelete! %d",
             left_mouse_command = "buffer %d",
             middle_mouse_command = nil,
-            indicator_icon = "▎",
+            indicator_icon = "",
             buffer_close_icon = "",
             modified_icon = "●",
             close_icon = "",
---
+++ .config/nvim/lua/EC2854/plugins/leap.lua
@@ -0,0 +1,7 @@
+return {
+    "ggandor/leap.nvim",
+    config = function ()
+        require('leap').create_default_mappings()
+    end
+}
+
