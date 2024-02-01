cfa96a7d4f67d99e813b88d901568e066147a6d6
Mleciu <igorpietrus@gmail.com>
2024-02-01 18:19:17 +0100

    funny plugins alttab + bars(useless but funni)

---
+++ .config/hypr/scripts/toggleplugin.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+get_state() {
+    local state=$(hyprpm list | grep -A 1 "$1" | tail -n 1 | awk -F ':' '{print $2}')
+    echo "$state"
+}
+
+plugin_name="$1"
+state=$(get_state "$plugin_name")
+
+if [[ "$state" == *"false"* ]]; then
+    hyprpm enable "$plugin_name" > /dev/null 2>&1
+else 
+    hyprpm disable "$plugin_name" > /dev/null 2>&1 
+fi
