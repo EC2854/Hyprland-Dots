0cb618c6d48e61a3e74ccca2cf849ff35106e0d8
EC2854 <ec2854@proton.me>
2024-02-19 15:17:04 +0100

    limited max size of title in a bar(ags).

--- .config/ags/modules/bar/media.js
+++ .config/ags/modules/bar/media.js
@@ -1,20 +1,21 @@
 import { Widget, Utils, Mpris } from "../../imports.js";
-const { execAsync } = Utils
+const { execAsync } = Utils;
 
 export const Media = () => Widget.Button({
     class_name: 'media',
-    on_primary_click: () => Mpris.getPlayer('')?.playPause(),
+    on_primary_click: () => Mpris.getPlayer('').playPause(),
     onSecondaryClickRelease: () => {
         execAsync(['bash', '-c', 'anyrun', '&']);
     },
-    on_scroll_up: () => Mpris.getPlayer('')?.next(),
-    on_scroll_down: () => Mpris.getPlayer('')?.previous(),
+    on_scroll_up: () => Mpris.getPlayer('').next(),
+    on_scroll_down: () => Mpris.getPlayer('').previous(),
     child: Widget.Label('-').hook(Mpris, self => {
         if (Mpris.players[0]) {
             const { track_title } = Mpris.players[0];
-            self.label = `${track_title}`;
+            self.label = track_title.length > 60 ? `${track_title.substring(0, 60)}...` : track_title;
         } else {
             self.label = 'Nothing is playing';
         }
     }, 'player-changed'),
 });
+
