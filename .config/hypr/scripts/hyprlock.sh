c484f87b608afcd32a6875388cdba1c7ec9f3e62
EC2854 <ec2854@proton.me>
2024-04-02 23:40:12 +0200

    updated hyprlock script

--- .config/hypr/scripts/hyprlock.sh
+++ .config/hypr/scripts/hyprlock.sh
@@ -1,5 +1,5 @@
 wallpaper=$(cat ~/.cache/swww/$(ls ~/.cache/swww | tail -n 1) | awk -F '%' '{print $1}')
 destination=~/Pictures/Wallpapers/hyprlock.png
 
-cp $wallpaper $destination
+ffmpeg -y -v 0 -i $wallpaper $destination
 hyprlock
