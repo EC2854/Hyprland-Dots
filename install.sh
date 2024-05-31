49f1b8499af6cb78d0859dcc1fd4614b94120675
EC2854 <ec2854@proton.me>
2024-05-31 02:35:13 +0200

    few tweaks to install script

--- install.sh
+++ install.sh
@@ -60,47 +60,21 @@ clone_repository() {
     local destination="$2"
 
     print_info "Cloning $repository_url..."
-    git clone --quiet "$repository_url" "$destination" && print_success "Cloned $destination successfully" && print_error "Error cloning $repository_url to $destination"
+    git clone --quiet "$repository_url" "$destination" && print_success "Cloned $destination successfully" || print_error "Error cloning $repository_url to $destination"
 }
 # copy function
-copy_folder() {
-    local source=("$1"/*)  # Capture the list of files and directories
-    local destination="$2"
-
-    ls "$destination" &>/dev/null || mkdir -p $destination
-
-    print_info "Copying files from $1 to $destination"
-    cp -f "${source[@]}" "$destination" && print_success "Files copied successfully to $destination" || print_error "Error while copying to $destination"
-}
-copy_files() {
+copy() {
     local source=$1 
     local destination="$2"
 
     print_info "Copying files from $1 to $destination"
-    cp -f "$source" "$destination" && print_success "Files copied successfully to $destination" && print_error "Error while copying to $destination"
+    cp -rf "$source" "$destination" && print_success "Files copied successfully to $destination" || print_error "Error while copying to $destination"
 }
 # Install Hyprland Plugins
 install_plugin() {
     local name=$1
-    
     hyprpm add $name && print_success "$name installed successfully" || print_error "Error while installing $name"
 }
-#Install Kitty Plugins
-# clone_kitty() {
-#     local  repository_url=$1
-#     
-#     print_info "Creating tmp folder"
-#     mkdir ./tmp && print_success "Folder Created successfully" # create tmp folder
-# 
-#     print_info "Cloning $repository_url..."
-#     git clone --quiet "$repository_url" && print_success "Cloned $repository_url successfully" && print_error "Error cloning $repository_url"
-#     print_info "Moving .py files to ~/.config/kitty/"
-#     mv -f ./tmp/*.py ~/.config/kitty/ && print_success "Files moved successfully" || print_error "Error moving files"
-# 
-#     print_info "Removing tmp folder"
-#     rm -rf "./tmp" && print_success "Folder removed successfully" # remove tmp folder
-# }
-
 # confirmation
 ask_for_confirmation() {
     print_warning "Caution: This script overwrites config files. Change resolution in ./config/hypr/hyprland.conf. RTFM: https://wiki.hyprland.org/Configuring/Monitors/"
@@ -114,34 +88,43 @@ ask_for_confirmation() {
     esac
 }
 # Check Distro
-command -v  pacman &>/dev/null || print_warning "This script is intended for Arch Linux. You can still run this script, but it won't install any packages." && not_arch_btw=true
+command -v  pacman &>/dev/null || { 
+    print_warning "This script is intended for Arch Linux. You can still run this script, but it won't install any packages." 
+    not_arch_btw=true 
+}
 
 # Check for paru
-command -v paru &>/dev/null || print_warning "paru not found. You can still run this script, but it won't install any packages." && not_arch_btw=true
+command -v paru &>/dev/null || { 
+    print_warning "paru not found. You can still run this script, but it won't install any packages." 
+    not_arch_btw=true 
+}
 
 # Check for internet connection
-ping -q -c 1 -W 1 github.com &>/dev/null || print_error "No internet connection. Exiting..." && exit 1
+ping -q -c 1 -W 1 github.com &>/dev/null || {
+    print_error "No internet connection. Exiting..." 
+    exit 1 
+}
 
 # Check Folder
-ls $(pwd)/install.sh &>/dev/null || print_error "Please open folder with this script before running it" && exit 1
+ls $(pwd)/install.sh &>/dev/null || { 
+    print_error "Please open folder with this script before running it" 
+    exit 1 
+}
 
 # Ask for confirmation before continuing
 ask_for_confirmation
 
-
-if [[ $not_arch_btw == false ]]; then
-    install_packages
-fi
+[[ $not_arch_btw == false ]] && install_packages
 
 # Copy files to ~/.config directory
-copy_folder ./.config ~/.config/
+copy ./.config ~/.config/
 
 # Copy .zshrc
-copy_files ./.zshrc ~/
+copy ./.zshrc ~/
 # Copy Bashrc
-copy_files ./.bashrc ~/
+copy ./.bashrc ~/
 
-# Clone zsh plugins
+# install zsh plugins
 clone_repository https://github.com/Aloxaf/fzf-tab ~/.config/zsh/fzf-tab
 clone_repository https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/zsh-autosuggestions
 clone_repository https://github.com/zsh-users/zsh-history-substring-search.git ~/.config/zsh/zsh-history-substring-search
@@ -152,20 +135,20 @@ clone_repository https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.con
 # clone_kitty https://github.com/yurikhan/kitty-smart-tab
 
 # Copy Wallpapers
-copy_folder ./Wallpapers ~/Pictures/Wallpapers/
+copy ./Wallpapers ~/Pictures/Wallpapers/
 
 # Quick fix for gtk 4
-ln -sf /usr/share/themes/Catppuccin-Mocha-Standard-Mauve-Dark/gtk-4.0 $HOME/.config/gtk-4.0
+ln -sf /usr/share/themes/Catppuccin-Mocha-Standard-Mauve-Dark/gtk-4.0 ~/.config/gtk-4.0
 
 # Hyprland Plugins
 print_info "Plugins time!"
 print_warning "this part can fail. i don't know what to do about it."
-print_info "if it will fail install hyprland-plugins and Hyprspace plugins manually"
+print_info "if it will fail install hyprland-plugins and Hyprspace plugins manually (or rerun script inside hyprland)"
 
 install_plugin https://github.com/hyprwm/hyprland-plugins
 install_plugin https://github.com/KZDKM/Hyprspace
 
 # Final message
-print_info "That's it! Uncopied stuff is: web folder"
+print_info "That's it!"
 print_info "Reboot ur system :3"
 print_warning "Run 'hyprpm enable Hyprspace' after installation"
